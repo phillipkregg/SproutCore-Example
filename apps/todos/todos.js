36483f25e70fac92bd23bcc3b71d54c5a15e7c64
PhillipKregg <phillipkregg@gmail.com>
2012-01-30 21:57:46 -0600

    Created ToDo list with SproutCore!

---
+++ apps/todos/todos.js
@@ -0,0 +1,113 @@
+// ==========================================================================
+// Project:   Todos
+// Copyright: @2012 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+
+
+Todos = SC.Application.create();
+
+Todos.Todo = SC.Object.extend({
+	title: null,
+	isDone: false
+});
+
+// updating previous code
+ 
+Todos.todoListController = SC.ArrayController.create({
+  // Initialize the array controller with an empty array.
+  content: [],
+ 
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  },
+
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('Length');
+  }.property('@each.isDone'),
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }    
+  }.property('@each.isDone'),
+  
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this);
+  }
+  
+});
+
+
+Todos.CreateTodoView = SC.TextField.extend({
+  insertNewline: function() {
+    var value = this.get('value');
+
+    if (value) {
+      Todos.todoListController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+
+
+Todos.MarkDoneView = SC.Checkbox.extend({
+  
+  titleBinding: '.parentView.content.title',
+  valueBinding: '.parentView.content.isDone'
+});
+
+
+Todos.StatsView = SC.TemplateView.extend({
+  remainingBinding: 'Todos.todoListController.remaining',
+
+  displayRemaining: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining')
+});
+
+
+
+// before SC.ready
+ 
+Todos.CreateTodoView = SC.TextField.extend({
+  insertNewline: function() {
+    var value = this.get('value');
+ 
+    if (value) {
+      Todos.todoListController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+
+
+SC.ready(function() {
+  Todos.mainPane = SC.TemplatePane.append({
+    layerId: 'todos',
+    templateName: 'todos'
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
